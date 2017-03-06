82b50363b6c99ded4468458a8a8ddc0ed4293c67
carolineh101 <cho19@stanford.edu>
2017-03-05 17:35:15 -0800

    Initial commit

---
+++ app/src/main/java/com/example/carolineho/hackoverflow_android_kit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.carolineho.hackoverflow_android_kit;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        setContentView(R.layout.activity_main);
+    }
+}
